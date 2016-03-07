a5dde603e0599b7c88c2e5f4db2cdc6ebbb9dde9
CARMEN MINIKUS <carmenminikus@CARMENs-MacBook-Pro.local>
2016-03-08 10:46:59 +1100

    change radiu 50%

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+
+var position ={
+  row1 : ["a","b","c"],
+  row2: ["d","e","f"],
+  row3: ["g","h","i"]
+};
+
+
+
+
+
+}); //end $(document.ready)
+
 console.log("test")
